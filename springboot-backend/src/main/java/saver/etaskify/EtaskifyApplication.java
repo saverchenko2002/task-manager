8702254da06abe55c6ccaaee4b2c334ba34076d8
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-09-11 21:28:47 +0300

    add initial project setup

---
+++ springboot-backend/src/main/java/saver/etaskify/EtaskifyApplication.java
@@ -0,0 +1,13 @@
+package saver.etaskify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtaskifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtaskifyApplication.class, args);
+    }
+
+}
---
+++ springboot-backend/src/test/java/saver/etaskify/EtaskifyApplicationTests.java
@@ -0,0 +1,13 @@
+package saver.etaskify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtaskifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
